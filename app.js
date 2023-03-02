75d84c25f893f2e8518bf10e712d5904a53ac427
Daniel Jong <danielj1415@gmail.com>
2023-03-02 13:05:42 -0800

    changed background and resized model

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-console.log(app);
 app.load('https://prod.spline.design/jPigzQBeDeK3N4h4/scene.splinecode');
+
+
+
+
